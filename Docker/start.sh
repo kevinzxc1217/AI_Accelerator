4f0b1d8739b8bb70c4a1305462c35d08cbdad998
Jench2103 <E24084208@gs.ncku.edu.tw>
2021-07-04 20:31:49 +0800

    將 ngrok 放入 image 並設定環境變數 $ngrok

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,3 +1,9 @@
+# Set $ngrok as an environment variable
+echo "export ngrok=/Docker/ngrok" >> ~/.bashrc
+echo "export CLASSPATH=.:$ngrok/lib:$ngrok/jre/lib:$CLASSPATH" >> ~/.bashrc
+echo "export PATH=$ngrok/bin:$ngrok/jre/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+
 # Move to /ProjectFiles
 cd /ProjectFiles
 
