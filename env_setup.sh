bcbeeb6f3d96f9c6039e926252afcf713ee311b0
weifen <weifen.lin@gs.ncku.edu.tw>
2021-07-18 03:23:18 +0800

    change username in the containe

--- env_setup.sh
+++ env_setup.sh
@@ -4,13 +4,13 @@ GIT_EMAIL=weifen.lin@gs.ncku.edu.tw
 GITLAB_LOGIN=weifen
 
 # docker configuration
-COURSE="base"
+COURSE="aias"
 PORT_MAPPING="3000:3000"                        # host:container
 NGINX_PORT="8080"                               # host
 
 # project parameters, must be consistent with gitlab URLs
 RUN_FLASK=false                                 # start docker env with / without uWSGI and nginx proxy
-COURSE_GITLAB="aica-spring-2020"
+COURSE_GITLAB="aias-spring-2021"
 PROJECT=""                   # projects without flask
 FLASK_PROJECT=""                # flask projects
 CURRENT_FLASK_FOLDER=""     # mount to /workspace/www in container
