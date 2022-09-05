fed124f7338945c2e9aec6bef394e9a5eb5f5016
Haouo <az589961@gmail.com>
2022-09-05 11:36:27 +0800

    Finish modification of AIAS Fall 2022 Docker image

--- env_setup.sh
+++ env_setup.sh
@@ -1,7 +1,7 @@
-# personal settings
-GIT_NAME="yenpo"
-GIT_EMAIL=yenpo.chen.ncku@gmail.com
-GITLAB_LOGIN=yenpo
+#  personal settings
+GIT_NAME="Haouo"
+GIT_EMAIL=e24096409@gs.ncku.edu.tw
+GITLAB_LOGIN=Haouo
 
 # docker configuration
 COURSE=aias
@@ -9,3 +9,5 @@ COURSE=aias
 # project parameters, must be consistent with gitlab URLs
 COURSE_GITLAB="aias_fall_2022"
 
+# project list
+# PROJECT="lab02 lab03 lab04 lab05 lab06 lab07 lab08 lab09 lab10 lab11 lab12 lab13 lab14 lab15 lab16"
--- run.sh
+++ run.sh
@@ -13,33 +13,33 @@ LABs=$(echo $PROJECT | tr "," "\n")
 for LAB in $LABs
 do
     cd "$ROOT"
-    
+
     # check if the repo exists
     git ls-remote https://playlab.computing.ncku.edu.tw:4001/$COURSE_GITLAB/$LAB.git -q > /dev/null 2>&1
     if [ $? != 0 ]; then
         echo -e "The project repo : $LAB does not exist\n"
         continue
     fi
-    
+
     # clone specified repo
     cd projects
     [ ! -d "./$LAB" ] && git clone https://playlab.computing.ncku.edu.tw:4001/$COURSE_GITLAB/$LAB.git
-    
+
     cd $LAB
-    
+
     # setup personal repo
     git config user.name $GIT_NAME
     git config user.email $GIT_EMAIL
-    
+
     # remove original upstream repo
     check=$(git remote -v | grep "playlab	https://playlab.computing.ncku.edu.tw:4001")
     if [[ $check ]]; then
         git remote remove playlab
     fi
     git remote add playlab https://playlab.computing.ncku.edu.tw:4001/$GITLAB_LOGIN/$LAB.git
-    
+
     git remote -v
-    
+
     # create new upstream playlab repo
     check=$(git ls-remote https://playlab.computing.ncku.edu.tw:4001/$GITLAB_LOGIN/$LAB.git)
     if [[ $check =~ "fatal" ]];
